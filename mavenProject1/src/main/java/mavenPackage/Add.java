21587ec715790a13b59d5b059a4e0a342c517636
akash <akash@LAPTOP-T539ER3H>
2024-11-28 10:34:57 +0530

    Add only using method

---
+++ mavenProject1/src/main/java/mavenPackage/Add.java
@@ -0,0 +1,19 @@
+package mavenPackage;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Add obj=new Add();
+		obj.addition(3,4);
+		
+
+	}
+	public void addition(int a,int b)
+	{
+		int c= a+b;
+		System.out.println(c);
+		
+	}
+
+}
